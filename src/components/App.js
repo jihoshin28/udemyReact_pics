0dddc013896b7402a11f2b87e7ab4d5b643cff60
geeuho <geeuho@gmail.com>
2020-06-16 14:58:24 -0700

    passed children helper fxns

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,23 @@
-import React from 'react'
+import React, { Component } from 'react'
+import SearchBar from './SearchBar'
 
-const App = () => {
-    return <div>App</div>
+class App extends Component {
+    constructor(){
+        super()
+    }
+    onFormSubmit = term => {
+        console.log(term)
+    }
+
+    render(){
+        return (
+            <div class="ui container" style={{ marginTop: '10px' }}>
+                App
+                <SearchBar onSubmit = {this.onFormSubmit} />
+            </div>
+        )
+    }
+    
 }
+
+export default App
---
+++ src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class SearchBar extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            input: ''
+        }
+    }
+
+    onFormSubmit = event => {
+        event.preventDefault()
+        this.props.onSubmit(this.state.input)
+    }
+
+    render(){
+        return (
+            <div class = "ui segment">
+                <form onSubmit = {this.onFormSubmit} class = "ui form">
+                    <div class = "field">
+                        <label> Image Search</label>
+                        <input type="text" onClick = {() => console.log(this.state.input)} value = {this.state.input} onChange = {(e)=> this.setState({input: e.target.value})}/>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
