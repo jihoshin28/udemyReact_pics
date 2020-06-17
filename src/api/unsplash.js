426c72a896d8691680c1a5f6423096347e3d8c1e
geeuho <geeuho@gmail.com>
2020-06-17 11:05:26 -0700

    url fixed

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://api.unsplash.com/search/photos',
+    baseURL: 'https://api.unsplash.com',
     headers: {
         Authorization: 
             'Client-ID kaR-BTgEPJ7y7bbfZHNxl1RSpgwfrcxn2zE-ki9Riz0'
