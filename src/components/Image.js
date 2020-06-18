485266fc08001ae6238173ecbd2bbe01c498e0eb
geeuho <geeuho@gmail.com>
2020-06-18 16:07:13 -0700

    grid implemented

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,18 +3,26 @@ import React from 'react'
 class Image extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
 
+        }
         this.imageRef = React.createRef()
     }
 
     componentDidMount(){
-        console.log(this.imageRef)
+        this.imageRef.current.addEventListener('load', this.setSpans);
     }
-    
+
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10 + 1);
+        this.setState({ spans })
+    }
+
     render() {
         return (
-            <div>
-                <img alt={this.props.alt} src={this.props.picURL}></img>
+            <div style = {{gridRowEnd: `span ${this.state.spans}`}}>
+                <img ref = {this.imageRef} alt={this.props.alt} src={this.props.picURL}></img>
             </div>
         )
     }
